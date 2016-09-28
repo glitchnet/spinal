52be63bf23918205ae3fb64cf110bfeabb277beb
Jen <jfong@mozilla.com>
2016-09-27 23:07:33 -0700

    space tunnel iteration

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
   var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
   var counter = 0;
-  var max = 10;
+  var max = 55;
   var width;
   var height;
-  var SCALEX = 82;
-  var SCALEY = -5;
+  var SCALEX = 92;
+  var SCALEY = 43;
 
   function reset() {
     ctx.clearRect(0, 0, width, height);
@@ -19,19 +19,21 @@
 
   function render() {
     ctx.beginPath();
-    ctx.lineWidth = 1 * (counter - 1);
-    ctx.strokeStyle = 'rgba(45, ' + Math.floor(Math.random() * counter * 5) + ', 155, 0.7)';
+    ctx.lineWidth = 5 * (counter - 1);
+    ctx.strokeStyle = 'rgba(25, ' + Math.floor(Math.random() * counter * 1) + ', 25, 0.1)';
     ctx.moveTo(counter, 0);
-    ctx.lineTo(100 * SCALEX, 10 * counter * SCALEY);
+    ctx.lineTo(10 * SCALEX, 110 * counter * SCALEY);
+    ctx.lineTo(100 * SCALEX, 140 * counter * SCALEY);
+    ctx.lineTo(-300 * SCALEX, -40 * counter * SCALEY);
     ctx.lineTo(100 * SCALEX, 40 * counter * SCALEY);
     ctx.stroke();
     ctx.closePath();
-    counter = counter + 8;
+    counter++;
 
     if (counter > max) {
-      ctx.translate(10 * SCALEX, 100 * SCALEY);
-      ctx.rotate(85 * (Math.PI / 180));
-      ctx.scale(1.15, 0.75);
+      ctx.translate(10 * SCALEX, 10 * SCALEY);
+      ctx.rotate(185 * (Math.PI / 180));
+      ctx.scale(0.75, 0.75);
       counter = 0;
     }
 
