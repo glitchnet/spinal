0a96ff45e6c97a8cfa7b92bbf1305835f48bd101
Jen <jfong@mozilla.com>
2016-09-27 22:31:33 -0700

    fourth iteration

--- main.js
+++ main.js
@@ -3,41 +3,39 @@
   var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
   var counter = 0;
-  var max = 10;
+  var max = 100;
   var width;
   var height;
-  var SCALEX = 6;
+  var SCALEX = 8;
   var SCALEY = 2;
 
   function reset() {
     ctx.clearRect(0, 0, width, height);
     width = window.innerWidth;
     height = window.innerHeight;
+    ctx.width = width;
+    ctx.height = height;
   }
 
   function render() {
     ctx.beginPath();
-    ctx.lineWidth = 3 * counter;
-    ctx.strokeStyle = 'rgba(255, ' + (counter % 255) + ', 130, ' + (0.1 * counter) + ')';
+    ctx.lineWidth = 1 * counter;
+    ctx.strokeStyle = 'rgba(5, ' + Math.floor(Math.random() * counter * 10) + ', 255, 0.2)';
     ctx.moveTo(0, 0);
     ctx.lineTo(100 * SCALEX, 10 * counter * SCALEY);
     ctx.lineTo(100 * SCALEX, 40 * counter * SCALEY);
     ctx.stroke();
     ctx.closePath();
     counter++;
-    //pos++;
+
     if (counter > max) {
-      //ctx.translate();
-      //ctx.scale(0.75, 0.75);
       ctx.translate(100 * SCALEX, 100 * SCALEY);
       ctx.rotate(105 * (Math.PI / 180));
-      ctx.scale(0.65, 0.65);
+      ctx.scale(0.75, 0.75);
       counter = 0;
     }
 
-    setTimeout(function () {
-      requestAnimationFrame(render);
-    }, 50);
+    requestAnimationFrame(render);
   }
 
   window.onresize = function () {
