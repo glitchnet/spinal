402735361972da1491e95d017c9953a515243e78
Jen <jfong@mozilla.com>
2016-09-27 21:50:51 -0700

    third iteration

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
   var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
   var counter = 0;
-  var max = 5;
+  var max = 10;
   var width;
   var height;
-  var x;
-  var y;
+  var SCALEX = 6;
+  var SCALEY = 2;
 
   function reset() {
     ctx.clearRect(0, 0, width, height);
@@ -17,11 +17,11 @@
 
   function render() {
     ctx.beginPath();
-    ctx.lineWidth = 2 * counter;
+    ctx.lineWidth = 3 * counter;
     ctx.strokeStyle = 'rgba(255, ' + (counter % 255) + ', 130, ' + (0.1 * counter) + ')';
     ctx.moveTo(0, 0);
-    ctx.lineTo(100, 10 * counter);
-    ctx.lineTo(100, 40 * counter);
+    ctx.lineTo(100 * SCALEX, 10 * counter * SCALEY);
+    ctx.lineTo(100 * SCALEX, 40 * counter * SCALEY);
     ctx.stroke();
     ctx.closePath();
     counter++;
@@ -29,7 +29,7 @@
     if (counter > max) {
       //ctx.translate();
       //ctx.scale(0.75, 0.75);
-      ctx.translate(100, 100);
+      ctx.translate(100 * SCALEX, 100 * SCALEY);
       ctx.rotate(105 * (Math.PI / 180));
       ctx.scale(0.65, 0.65);
       counter = 0;
